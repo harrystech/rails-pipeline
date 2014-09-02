dd0e7a812ca841ab237ec800435daf8a16cfa7ae
Akeem Adeniji <akeem.adeniji@gmail.com>
2014-09-02 15:44:31 -0400

    Base64 encoding/decoding protocol buffers
    After encountering serialization issue with the raw payload and
    IronMQ’s expectation that the payload will be json, moving the payload
    to an intermediate data format is required.  The decision to use B64
    was based on the following stackoverflow post:
    http://stackoverflow.com/questions/1443158/binary-data-in-json-string-so
    mething-better-than-base64

--- lib/rails-pipeline/ironmq_subscriber.rb
+++ lib/rails-pipeline/ironmq_subscriber.rb
@@ -1,6 +1,7 @@
 # Sinatra endpoints for consuming IronMQ push queues for the rails-pipeline
 
 require 'sinatra'
+require 'base64'
 
 module RailsPipeline
   class IronmqSubscriber < Sinatra::Base
@@ -9,7 +10,7 @@ module RailsPipeline
     post '/' do
       t0 = Time.now
       data = request.body.read
-      envelope = RailsPipeline::EncryptedMessage.parse(data)
+      envelope = RailsPipeline::EncryptedMessage.parse(Base64.strict_decode64(data))
       handle_envelope(envelope)
       t1 = Time.now
       RailsPipeline.logger.debug "Consuming from IronMQ: #{envelope.topic} took #{t1-t0}s"
