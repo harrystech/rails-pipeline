e1d63f3d723db372963cefe9b170dcda928bea43
Akeem Adeniji <akeem.adeniji@gmail.com>
2014-11-13 17:13:19 -0500

    Refactor method signature for payload params
    As part of making this emitter easier to use, I have added some basic
    documentation and have collapsed the parameters associated with the
    message payload

--- lib/rails-pipeline/basic_emitter.rb
+++ lib/rails-pipeline/basic_emitter.rb
@@ -4,19 +4,21 @@ module RailsPipeline
     class BasicEmitter
         include RailsPipeline::SymmetricEncryptor
 
-        def self.emit(topic, type_info, payload, version, event_type, publisher)
+        # @param [Hash] params Message fields for message generation
+        # @option params [String] :topic
+        # @option params [String] :type_info
+        # @option params [String] :payload
+        # @option params [String] :version
+        # @option params [Integer] :event_type
+        # @param [Publisher] An object that responds to publish
+        def self.emit(params, publisher)
             if ENV.has_key?("DISABLE_RAILS_PIPELINE") || ENV.has_key?("DISABLE_RAILS_PIPELINE_EMISSION")
                 RailsPipeline.logger.debug "Skipping outgoing pipeline messages (disabled by env vars)"
                 return
             end
 
             begin
-                enc_data = create_message(topic,
-                                          type_info,
-                                          payload,
-                                          version,
-                                          RailsPipeline::EncryptedMessage::EventType::CREATED)
-
+                enc_data = create_message(params)
                 publisher.publish(enc_data.topic, enc_data.to_s)
             rescue Exception => e
                 RailsPipeline.logger.error("Error during emit(): #{e}")
@@ -25,12 +27,23 @@ module RailsPipeline
             end
         end
 
-        def self.create_message(topic, type_info, payload, version, event_type)
-            RailsPipeline.logger.debug "Emitting to #{topic}"
-            self.encrypt(payload,
-                               type_info: type_info,
-                               topic: topic,
-                               event_type: event_type)
+
+
+        # Created an encryped message with the provided payload data and associated
+        # metadata.
+        #
+        # @param [Hash] params Message fields for message generation
+        # @option params [String] :topic
+        # @option params [String] :type_info
+        # @option params [String] :payload
+        # @option params [String] :version
+        # @option params [Integer] :event_type
+        def self.create_message(params)
+            RailsPipeline.logger.debug "Emitting to #{params[:topic]}"
+            self.encrypt(params[:payload],
+                         type_info: params[:type_info],
+                         topic: params[:topic],
+                         event_type: params[:event_type])
         end
 
     end
